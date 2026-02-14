b393f2acd297c4f4e6f67d7527dd8df90bbf7543
Jacob Schlecht <dadadah@echoha.us>
2026-02-13 20:57:53 -0700

    Add the ability to choose voice server based on the server description

--- packages/client/components/rtc/state.tsx
+++ packages/client/components/rtc/state.tsx
@@ -120,7 +120,16 @@ class Voice {
     room.addListener("disconnected", () => this.#setState("DISCONNECTED"));
 
     if (!auth) {
-      auth = await channel.joinCall("worldwide");
+      let voiceServer = "worldwide";
+      if (channel.server?.description) {
+        const descSplits = channel.server.description.split("\n");
+        const lastLine = descSplits[descSplits?.length - 1];
+        if (lastLine.startsWith("voice-server:")) {
+          voiceServer = lastLine.replace("voice-server:", "");
+          console.log(voiceServer);
+        }
+      }
+      auth = await channel.joinCall(voiceServer);
     }
 
     await room.connect(auth.url, auth.token, {
