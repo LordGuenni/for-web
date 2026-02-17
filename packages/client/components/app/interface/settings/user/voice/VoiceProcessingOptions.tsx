feb30a4436c557cc3e084be6b8ae73d3fcc5373c
LordGuenni <user@stamer.dev>
2026-02-17 22:38:03 +0100

    Fix RNN Button / Toggle

--- packages/client/components/app/interface/settings/user/voice/VoiceProcessingOptions.tsx
+++ packages/client/components/app/interface/settings/user/voice/VoiceProcessingOptions.tsx
@@ -1,11 +1,8 @@
 import { Trans } from "@lingui-solid/solid/macro";
-import { batch } from "solid-js"; 
+import { batch } from "solid-js";
 import { useState } from "@revolt/state";
 import { CategoryButton, Checkbox, Column, Text } from "@revolt/ui";
 
-/**
- * Voice processing options
- */
 export function VoiceProcessingOptions() {
   const state = useState();
 
@@ -15,46 +12,46 @@ export function VoiceProcessingOptions() {
         <Trans>Voice Processing</Trans>
       </Text>
       <CategoryButton.Group>
+        {/* Browser Noise Suppression */}
         <CategoryButton
           icon="blank"
-          action={<Checkbox checked={state.voice.noiseSupression} />}
+          // pointer-events: none prevents the checkbox from intercepting clicks
+          action={<Checkbox checked={state.voice.noiseSupression} style={{ "pointer-events": "none" }} />}
           onClick={() => {
             batch(() => {
               const nextValue = !state.voice.noiseSupression;
               state.voice.noiseSupression = nextValue;
-              // If we turn on Browser suppression, turn off RNNoise
-              if (nextValue) {
-                state.voice.rnnoise = false;
-              }
+              if (nextValue) state.voice.rnnoise = false;
             });
           }}
         >
           <Trans>Browser Noise Supression</Trans>
         </CategoryButton>
-        
+
+        {/* RNNoise */}
         <CategoryButton
           icon="blank"
-          action={<Checkbox checked={state.voice.rnnoise} />}
-          onClick={() => {
+          action={<Checkbox checked={state.voice.rnnoise} style={{ "pointer-events": "none" }} />}
+          onClick={(e) => {
+            // Stop the checkbox from double-triggering
+            e.preventDefault(); 
             batch(() => {
               const nextValue = !state.voice.rnnoise;
               state.voice.rnnoise = nextValue;
-              // If we turn on RNNoise, turn off Browser suppression
-              if (nextValue) {
-                state.voice.noiseSupression = false;
-              }
+              if (nextValue) state.voice.noiseSupression = false;
             });
           }}
         >
           <Trans>Enhanced Noise Suppression Powered by RNNoise</Trans>
         </CategoryButton>
 
+        {/* Echo Cancellation */}
         <CategoryButton
           icon="blank"
-          action={<Checkbox checked={state.voice.echoCancellation} />}
-          onClick={() =>
-            (state.voice.echoCancellation = !state.voice.echoCancellation)
-          }
+          action={<Checkbox checked={state.voice.echoCancellation} style={{ "pointer-events": "none" }} />}
+          onClick={() => {
+            state.voice.echoCancellation = !state.voice.echoCancellation;
+          }}
         >
           <Trans>Browser Echo Cancellation</Trans>
         </CategoryButton>
